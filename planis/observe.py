0bf405720d760a9ec682d7202be00a7243999f3c
Ichiro Yoshida <ichiro1@msb.biglobe.ne.jp>
2018-08-27 06:18:13 +0900

    Manj180827

--- planis/observe.py
+++ planis/observe.py
@@ -2,8 +2,8 @@ import ephem
 
 obs = ephem.Observer()
 
-obs.name = '愛光学園'
-obs.lon = '132.7332'
-obs.lat = '33.8460'
+obs.name = '福岡'
+obs.lon = '130.390'
+obs.lat = '33.593'
 obs.date = '2018/09/01 21:00:00'
 obs.date -= 9*ephem.hour
