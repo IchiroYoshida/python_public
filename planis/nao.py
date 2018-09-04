04b182a68459b2a2532d2dfc435f75629014e5d6
IchiroYoshida <ichiro1@msb.biglobe.ne.jp>
2018-09-04 16:39:39 +0900

    Umi180904

--- planis/nao.py
+++
@@ -1,9 +0,0 @@
-import ephem
-
-obs = ephem.Observer()
-
-obs.name = '直島'
-obs.lon = '133.9750'
-obs.lat = '34.4597'
-obs.date = '2018/09/13 19:00:00'
-obs.date -= 9*ephem.hour
--- planis/observe.py
+++ planis/observe.py
@@ -2,8 +2,8 @@ import ephem
 
 obs = ephem.Observer()
 
-obs.name = '直島'
-obs.lon = '133.9750'
-obs.lat = '34.4597'
-obs.date = '2018/09/13 19:00:00'
+obs.name = '福岡'
+obs.lon = '130.390'
+obs.lat = '33.593'
+obs.date = '2018/09/04 21:00:00'
 obs.date -= 9*ephem.hour
