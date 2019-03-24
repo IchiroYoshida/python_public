5f32fffa08a992f5f0c466cfc50515cd592d8745
Ichiro Yoshida <ichiro1@msb.biglobe.ne.jp>
2019-03-25 06:00:10 +0900

    Manj190325

--- planisJ/observe.py
+++ planisJ/observe.py
@@ -2,8 +2,8 @@ import ephem
 
 obs = ephem.Observer()
 
-obs.name = '東京'
-obs.lon = '139.6010'
-obs.lat = '35.6694'
-obs.date = '2019/02/25 5:05:00'
+obs.name = '福岡'
+obs.lon = '130.390'
+obs.lat = '33.593'
+obs.date = '2019/03/21 19:07:00'
 obs.date -= 9*ephem.hour
