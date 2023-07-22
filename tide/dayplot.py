eeb9357745266601fc761297cf844a5d4d7b9843
Ichiro Yoshida <yoshida.ichi@gmail.com>
2023-07-22 09:10:49 +0900

    Pro230722

--- tide/dayplot.py
+++ tide/dayplot.py
@@ -18,7 +18,7 @@ matplotlib.rcParams['font.family'] = 'IPAexGothic'
 
 name = '石垣'
 #date = datetime.date.today().strftime('%Y/%m/%d')
-date = '2023/07/26'
+date = '2023/07/22'
 
 td3 = td.TD3(name)
 pt = h6.Tide(td3,date)
