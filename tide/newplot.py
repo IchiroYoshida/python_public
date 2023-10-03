aab02169a32cfe0152e96bf37c583e1cc91879d7
ichiroyoshida <ichiro1@msb.biglobe.ne.jp>
2023-10-03 13:15:07 +0900

    M1231003

--- tide/newplot.py
+++ tide/newplot.py
@@ -17,25 +17,26 @@ import seaborn as sns
 matplotlib.rcParams['font.family'] = 'IPAexGothic'
 
 name = '石垣'
-date = datetime.date.today().strftime('%Y/%m/%d')
+#date = datetime.date.today().strftime('%Y/%m/%d')
+date = '2023/10/01'
 
 td3 = td.TD3(name)
 pt = h6.Tide(td3,date)
 me = ME.MoonEph(date)
 
 date_prn = name+'   '+date+'  '+'  月齢  '+me.moon_age+' 日  '+me.tide_name+'潮'
-t2 = pt.tide[(date+"  8:00"):(date+"  20:00")]
+t2 = pt.tide[(date+"  6:00"):(date+"  18:00")]
 
-eb2 = pt.ebb[(date+"  8:00"):(date+"  20:00")]
-fl2 = pt.flow[(date+"  8:00"):(date+"  20:00")]
+eb2 = pt.ebb[(date+"  6:00"):(date+"  18:00")]
+fl2 = pt.flow[(date+"  6:00"):(date+"  18:00")]
 
 EbbLevels = eb2.values
 EbbTimes  = eb2.index
 FlowLevels = fl2.values
 FlowTimes  = fl2.index
 
-Tmin = datetime.datetime.strptime(date+"  7:45",'%Y/%m/%d %H:%M')
-Tmax = datetime.datetime.strptime(date+" 20:15",'%Y/%m/%d %H:%M')
+Tmin = datetime.datetime.strptime(date+"  5:45",'%Y/%m/%d %H:%M')
+Tmax = datetime.datetime.strptime(date+" 18:15",'%Y/%m/%d %H:%M')
 
 fig, ax = plt.subplots()
 sns.lineplot(data=t2, color = 'blue', ax=ax)
@@ -50,15 +51,15 @@ ax.xaxis.set_major_formatter(formatter)
 offset = 5 
 td =datetime.timedelta(minutes=30)
 
-#08:00 Left
-ti = datetime.datetime.strptime(date+ "   8:00",'%Y/%m/%d %H:%M')
-hi = t2[(date + "  8:00")]
+#06:00 Left
+ti = datetime.datetime.strptime(date+ "   6:00",'%Y/%m/%d %H:%M')
+hi = t2[(date + "  6:00")]
 StrLevel ='{:.0f}'.format(hi)+' (cm)'
 ax.text(ti, hi-offset, StrLevel)
 
-#20:00 Right
-ti = datetime.datetime.strptime(date+ "  20:00",'%Y/%m/%d %H:%M')
-hi = t2[(date + " 20:00")]
+#18:00 Right
+ti = datetime.datetime.strptime(date+ "  18:00",'%Y/%m/%d %H:%M')
+hi = t2[(date + " 18:00")]
 StrLevel ='{:.0f}'.format(hi)+' (cm)'
 ax.text(ti, hi-offset, StrLevel)
                                    
