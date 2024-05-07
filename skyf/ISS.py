4799b69a28c3204a2b40e891e9d75eafb53baa95
ichiroyoshida <ichiro1@msb.biglobe.ne.jp>
2024-05-07 16:15:34 +0900

    Umi240507

--- skyf/ISS.py
+++ skyf/ISS.py
@@ -13,17 +13,18 @@ eph = load('de421.bsp')
 sun, earth = eph['sun'], eph['earth']
 
 # 観測地設定
-fukuoka = wgs84.latlon(33.5930, 130.3903)
+#fukuoka = wgs84.latlon(33.5930, 130.3903)
+umi = wgs84.latlon(33.55923, 130.54263)
 
 # 国際宇宙ステーションの軌道要素設定
 satellites = load.tle('http://celestrak.com/NORAD/elements/stations.txt')
 iss = satellites['ISS (ZARYA)']
 
 # 指定時刻間に福岡上空を通るパスの検索
-t, events = iss.find_events(fukuoka, t0, t1, altitude_degrees=10.0)
+t, events = iss.find_events(umi, t0, t1, altitude_degrees=10.0)
 
 # 太陽高度、および太陽光が当たっているかの計算
-sun_alt = (earth + fukuoka).at(t).observe(sun).apparent().altaz()[0].degrees
+sun_alt = (earth + umi).at(t).observe(sun).apparent().altaz()[0].degrees
 sun_lit = iss.at(t).is_sunlit(eph)
 
 # 国際宇宙ステーションが見える条件の判定 
